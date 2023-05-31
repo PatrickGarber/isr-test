8ce463662e97c14df186bdc1ebf8175b87ae339b
Patrick Garber <Patrick.Garber@atriaseniorliving.com>
2023-05-31 10:55:26 -0500

    Update

---
+++ pages/posts/index.vue
@@ -0,0 +1,13 @@
+<template>
+    <div>
+      <h1>Posts</h1>
+      <ul>
+        <li v-for="post in posts" :key="post.id">
+          <nuxt-link :to="`/posts/${post.id}`">
+            {{ post.title }}
+          </nuxt-link>
+        </li>
+      </ul>
+    </div>
+  </template>
+  
