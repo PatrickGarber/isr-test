795a0f56ec8e9e0d7c36db10578628d85d75b53c
PatrickGarber <102746922+PatrickGarber@users.noreply.github.com>
2023-05-31 09:29:06 -0500

    Initial commit

---
+++ nuxt.config.ts
@@ -0,0 +1,7 @@
+export default {
+  // ...
+  target: 'static',
+  generate: {
+    interval: 2000, // Configure the regeneration interval as desired
+  },
+};
---
+++ pages/posts.vue
@@ -0,0 +1,24 @@
+<template>
+  <div>
+    <h1>Posts</h1>
+    <ul>
+      <li v-for="post in posts" :key="post.id">
+        {{ post.title }}
+      </li>
+    </ul>
+  </div>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      posts: [],
+    };
+  },
+  async created() {
+    const response = await fetch('https://jsonplaceholder.typicode.com/posts');
+    this.posts = await response.json();
+  },
+};
+</script>
