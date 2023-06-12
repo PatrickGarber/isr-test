5a28e062c73549af878443ce7b04363817107d7b
Patrick Garber <Patrick.Garber@atriaseniorliving.com>
2023-06-12 13:49:34 -0500

    Update

--- pages/posts/[_id].vue
+++ pages/posts/[_id].vue
@@ -1,22 +1,25 @@
 <template>
-  <div>
+  <div class="post">
     <div v-if="loading">Loading...</div>
     <div v-else-if="error">Error: {{ error }}</div>
     <div v-else>
-      <h1>{{ post?.title }}</h1>
-      <p>{{ post?.body }}</p>
+      <img :src="post.image" :alt="post.title" class="post-image" />
+      <h1>{{ post.title }}</h1>
+      <p>{{ post.body }}</p>
+      <p>Post ID: {{ post.id }}</p>
     </div>
   </div>
 </template>
 
 <script lang="ts">
 import { defineComponent } from 'vue';
-import postsData from '../../static/posts.json'; // Import the local JSON file
+import postsData from '@/static/posts.json'; // Import the local JSON file
 
 interface Post {
   id: string;
   title: string;
   body: string;
+  image: string;
 }
 
 export default defineComponent({
@@ -41,3 +44,26 @@ export default defineComponent({
   },
 });
 </script>
+
+<style scoped>
+.post {
+  max-width: 600px;
+  margin: 0 auto;
+  padding: 20px;
+}
+
+.post-image {
+  width: 100%;
+  height: auto;
+  margin-bottom: 10px;
+}
+
+h1 {
+  font-size: 24px;
+  margin-bottom: 10px;
+}
+
+p {
+  margin-bottom: 10px;
+}
+</style>
